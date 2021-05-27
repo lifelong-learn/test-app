782bc5e31f950591a27afc547e00740d5bc5b064
lifelong-learn <andreas@callutheran.edu>
2021-05-27 17:56:39 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload this and see what happens.
         </p>
         <a
           className="App-link"
